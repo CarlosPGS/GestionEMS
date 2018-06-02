046077f8de3ea61200227532d878f28511a60091
Daniel Rodríguez Alcalá <daniel.rodriguez.alcala@alumnos.upm.es>
2018-06-02 10:24:54 +0200

    añadir modulo Matriculas

---
+++ AppGestionEMS/AppGestionEMS/Models/GrupoClases.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppGestionEMS.Models
+{
+    public class GrupoClases
+    {
+        public enum TurnoType
+        {
+            Mañana,
+            Tarde
+        }
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public int numAlumnos { get; set; }
+        public TurnoType turno { get; set; }
+    }
+}
